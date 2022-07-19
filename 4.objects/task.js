d92fa9f84a8dfc86a639e0c910b57f26bf985ef3
nilifv <filinn14@gmail.com>
2022-07-19 10:20:10 +0300

    classes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,26 +1,25 @@
-function Student(name, gender, age) {
-    this.mame = name;
-    this.gender = gender;
-    this.age = age;
 
+function Student(name, gender, age) {
+  this.name = name
+  this.gender = gender
+  this.age = age
 }
 
-
 Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
+ this.subject = subjectName;
 }
 
 Student.prototype.addMark = function (mark) {
-  if (this.marks === undefined) {
+  if(this.marks === undefined) {
     this.marks = [];
-    this.marks.push(mark);
+    this.marks.push(mark)
   } else {
-    this.marks.push(mark);
+    this.marks.push(mark)
   }
 }
 
 Student.prototype.addMarks = function (...marks) {
-  if (this.marks === undefined) {
+  if(this.marks === undefined) {
     this.marks = [];
     this.marks.push(...marks)
   } else {
@@ -32,8 +31,8 @@ Student.prototype.getAverage = function () {
   return this.marks.reduce((sum, amount) => sum += amount) / this.marks.length;
 }
 
-Student.prototype.exclude = function(reason) {
-  delete this.subject;
-  delete this.marks;
+Student.prototype.exclude = function (reason) {
+  delete this.subject
+  delete this.marks
   this.excluded = reason;
 }
